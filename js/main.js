b4e59ab35ce688950abbf692a981e1e30ba3d476
Yang Su <suyang1+services@gmail.com>
2013-01-18 17:25:29 -0500

    Added spiral generating code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,129 @@
 var stats, scene, renderer, composer;
 var camera, cameraControl;
 
+var config = {
+  radius: 2,
+  tubeRadius: 0.01,
+
+  height: 0,
+
+  lengthSegments: 500,
+  radialSegments: 50,
+
+  revolutions: 1
+};
+
+THREE.HelixCurve = THREE.Curve.create(function() {},
+    function(t) {
+      var t2 = 2 * Math.PI * t * config.revolutions;
+      var tx = Math.cos(t2) * config.radius * t,
+          ty = Math.sin(t2) * config.radius * t,
+          tz = config.height * t;
+
+      return new THREE.Vector3(tx, ty, tz);
+    });
+
+THREE.Tube = function(path) {
+  this.geometry = new THREE.Geometry();
+
+  this.grid = new Array(config.lengthSegments);
+  this.path = path;
+
+  var tang = new THREE.Vector3();
+  var binormal = new THREE.Vector3();
+  var normal = new THREE.Vector3();
+  var pos = new THREE.Vector3();
+  var epsilon = 0.001;
+  var u, v;
+  var p1, p2;
+  var cx, cy;
+  var oldB;
+
+  for (var i = 0; i < config.lengthSegments; ++i) {
+
+    this.grid[i] = new Array(config.radialSegments);
+
+    u = i / config.lengthSegments;
+
+    var pos = this.path.getPointAt(u);
+    tang = this.path.getTangentAt(u);
+
+    if (oldB === undefined) {
+      //arbitrary vector
+      oldB = new THREE.Vector3(Math.random(), Math.random(), Math.random()).normalize();
+    }
+    normal.cross(oldB, tang).normalize();
+    binormal.cross(tang, normal).normalize();
+    oldB = binormal;
+
+    // // --------------------------------------------------------------------------------------------
+    // if (i == 0) normal = p1.clone().normalize(); // This is a total hack. Won't work if p1 is zero.
+    // // This is not production quality, either.
+    // // --------------------------------------------------------------------------------------------
+    // binormal.cross(tang, normal);
+    // normal.cross(binormal, tang);
+    // binormal.normalize();
+    // normal.normalize();
+    for (var j = 0; j < config.radialSegments; ++j) {
+
+      cx = config.tubeRadius * Math.cos(v); // TODO: Hack: Negating it so it faces outside.
+
+      cy = config.tubeRadius * Math.sin(v);
+
+      v = j / config.radialSegments * 2 * Math.PI;
+
+
+      var rCosN = new THREE.Vector3();
+      rCosN.copy(normal);
+      rCosN.multiplyScalar(cx);
+
+      var rSinB = new THREE.Vector3();
+      rSinB.copy(binormal);
+      rSinB.multiplyScalar(cy);
+
+      var pt = new THREE.Vector3();
+      pt.addSelf(pos);
+      pt.addSelf(rCosN);
+      pt.addSelf(rSinB);
+
+      this.grid[i][j] = this.vert(pt.x, pt.y, pt.z);
+    }
+  }
+
+  for (var i = 0; i < config.lengthSegments - 1; ++i) {
+
+    for (var j = 0; j < config.radialSegments; ++j) {
+
+      var ip = (i + 1) % config.lengthSegments;
+      var jp = (j + 1) % config.radialSegments;
+
+      var a = this.grid[i][j];
+      var b = this.grid[ip][j];
+      var c = this.grid[ip][jp];
+      var d = this.grid[i][jp];
+
+      var uva = new THREE.UV(i / config.lengthSegments, j / config.radialSegments);
+      var uvb = new THREE.UV((i + 1) / config.lengthSegments, j / config.radialSegments);
+      var uvc = new THREE.UV((i + 1) / config.lengthSegments, (j + 1) / config.radialSegments);
+      var uvd = new THREE.UV(i / config.lengthSegments, (j + 1) / config.radialSegments);
+
+      this.geometry.faces.push(new THREE.Face4(a, b, c, d));
+      this.geometry.faceVertexUvs[0].push([uva, uvb, uvc, uvd]);
+
+    }
+  }
+
+  this.geometry.computeCentroids();
+  this.geometry.computeFaceNormals();
+  this.geometry.computeVertexNormals();
+
+  return this;
+};
+
+THREE.Tube.prototype.vert = function(x, y, z) {
+  return this.geometry.vertices.push(new THREE.Vertex(new THREE.Vector3(x, y, z))) - 1;
+};
+
 var init = _.once(function() {
   if (Detector.webgl) {
     renderer = new THREE.WebGLRenderer({
@@ -42,18 +165,24 @@ var init = _.once(function() {
     document.getElementById('inlineDoc').innerHTML += '- <i>f</i> for fullscreen';
   }
 
-  // here you add your objects
-  // - you will most likely replace this part by your own
   var light = new THREE.AmbientLight(Math.random() * 0xffffff);
   scene.add(light);
 
-  var geometry = new THREE.TorusGeometry(1, 0.42, 16, 16);
+  var tube = new THREE.Tube(new THREE.HelixCurve());
+
+  // var mesh = THREE.SceneUtils.createMultiMaterialObject(geometry, [new THREE.MeshNormalMaterial(), new THREE.MeshNormalMaterial()]);
   var material = new THREE.MeshNormalMaterial();
-  var mesh = new THREE.Mesh(geometry, material);
+  var mesh = new THREE.Mesh(tube.geometry, material);
+  mesh.position.set(0, 0, 0);
+  mesh.rotation.set(0, Math.PI, 0);
+  mesh.scale.set(1, 1, 1);
   scene.add(mesh);
 
-  var receiver = {};
+
   var gui = new dat.GUI();
+
+  gui.add(config, 'radius', 1, 100);
+  gui.add(config, 'height', 1, 200);
 
 });
 
